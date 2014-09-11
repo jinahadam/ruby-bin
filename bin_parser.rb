5fe7c097015e587670f309ec2108d3d0972dd9d7
Jinah Adam <jinahadam@me.com>
2014-09-11 16:04:57 +0800

    Bin Parser Header

---
+++ bin_parser.rb
@@ -0,0 +1,60 @@
+class BinParser
+  
+  HEAD_BYTE1 = 0xA3
+  HEAD_BYTE2 = 0x95
+  
+  
+   def initialize(uri)
+    
+     file = File.open(uri) 
+     log_step = 0
+     file.each_byte { |c| 
+       
+       case log_step
+       when 0
+         if c == HEAD_BYTE1
+           log_step = log_step + 1
+         end
+       when 1
+         if c == HEAD_BYTE2
+           log_step = log_step + 1
+         else
+           log_step = 0
+         end
+       when 2
+         log_step = 0
+         line = log_entry(c,file)
+         #print c, ' ' 
+       else
+         puts "bad binary" 
+       end
+       
+     }
+     
+   end
+   
+   
+   def log_entry(packettype,file)
+     case packettype
+     when 0x80 #FMT
+       len = 86
+       
+       data = file.read(len)
+       type =  data[0..1].unpack('C')[0]
+       length =  data[1..2].unpack('C')[0]
+       name = data[2..5].unpack('Z*')[0]
+       format = data[5..21].unpack('Z*')
+       labels = data[21..86].unpack('Z*')
+       
+       puts "FMT, #{type}, #{length}, #{name}, #{format}, #{labels}"
+
+     else
+     
+     end
+   end
+   
+
+    
+   
+    
+end
